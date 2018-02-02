b29ea849cf6bfb2299a1626d1964b144cd37cc17
Konrad Belzowski <konrad.belzowski@gmail.com>
2018-02-02 03:27:43 -0800

    Secound Commit

---
+++ Allle/Functionalities/BaseView/BaseViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewViewController.swift
+//  Allle
+//
+//  Created by Belzi on 11/01/2018.
+//  Copyright © 2018 Belzi. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
